ee160e092f2b47f5876fc8bac552bb74d3f4c14a
AoifeHughes <ahughes@turing.ac.uk>
2024-01-12 12:59:02 +0000

    Add new files and update existing files

---
+++ main.py
@@ -0,0 +1,16 @@
+import DQN
+import torch
+import memory 
+def main():
+    rom_path = 'Pokemon - Crystal Version.gbc'
+    locations = {6: "DownstairsPlayersHouse", 0: "UpstairsPlayersHouse", 4: "OutsideStartingArea"}
+    location_address = 0xD148
+    device = torch.device("mps")
+    SCALE_FACTOR = 0.25
+    USE_GRAYSCALE = False
+    goal_loc = memory.outside_house
+    model = DQN.LearnGame(rom_path, locations, location_address, device, SCALE_FACTOR, USE_GRAYSCALE, goal_loc)
+    model.run()
+
+if __name__ == '__main__':
+    main()
