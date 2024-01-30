fe9755a0d0e6c78ce107212a45f0b14cd0c779de
AoifeHughes <ahughes@turing.ac.uk>
2024-01-30 12:54:03 +0000

    Update timeouts and nsteps in main.py

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ def main():
     device = torch.device("mps")
     SCALE_FACTOR = 1
     USE_GRAYSCALE = False
-    timeouts = [10, 50, 100, 500, 1000]
-    num_episodes = 100
+    timeouts = [1000]
+    num_episodes = 1000
     cpus = (os.cpu_count()-1) if device == torch.device("cpu") else 1
     episodes_per_batch = 1*cpus
-    batch_size = 16
-    nsteps = 3
+    nsteps = 5
+    batch_size = timeouts[0]//nsteps
     run(rom_path, device, SCALE_FACTOR, USE_GRAYSCALE, timeouts, num_episodes, episodes_per_batch, batch_size, nsteps, cpus=cpus)
 
 
