3ec5f0bea137a09791afc10fdcc5cbd9bfbaab89
AoifeHughes <ahughes@turing.ac.uk>
2024-02-09 18:39:31 +0000

    added stuff

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
-
 for ((i=1; i<=100; i++))
 do
+    # Check if the directory already exists
+    if [ -d "run_$i" ]; then
+        echo "Directory run_$i already exists, skipping..."
+        continue
+    fi
+
     python main.py --device mps --model RainbowDQN --checkpoint_interval 50000 --epsilon_by_location --num_episodes 1000 --sight 
     mkdir run_$i
     mv results run_$i
