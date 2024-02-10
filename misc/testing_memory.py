ef02da79d5d2c1b6849b989e9fcb435f18ae9f64
AoifeHughes <ahughes@turing.ac.uk>
2024-02-10 15:10:33 +0000

    Update image processing and rewards logic

--- misc/testing_memory.py
+++ misc/testing_memory.py
@@ -1,30 +1,23 @@
-# -*- coding: utf-8 -*-
 from pyboy import PyBoy
 import numpy as np
-
 from PIL import Image
 import pytesseract
-
+from collections import OrderedDict
+import matplotlib.pyplot as plt
+import os 
 
 def preprocess_image(image):
-    # Scale the image by a factor of 2
     original_size = image.size
     scaled_size = tuple(2 * x for x in original_size)
     image = image.resize(scaled_size, Image.LANCZOS)
-    # Convert to grayscale for better processing
     image = image.convert("L")
-    # Apply threshold to get a binary image
-    threshold_value = 120  # You might need to adjust this
+    threshold_value = 120
     image = image.point(lambda p: p > threshold_value and 255)
-    # Additional filters can be applied if necessary
-    # image = image.filter(ImageFilter.MedianFilter())
     return image
-
 
 def extract_text(image):
     text = pytesseract.image_to_string(image, config="--psm 11")
     return text
-
 
 def read_little_endian(pyboy, start, end):
     raw_bytes = []
@@ -33,10 +26,8 @@ def read_little_endian(pyboy, start, end):
         raw_bytes.append(byte)
     return raw_bytes
 
-
 def bytes_to_int(byte_list):
     return int.from_bytes(byte_list, byteorder="little")
-
 
 def count_set_bits(byte):
     count = 0
@@ -45,29 +36,45 @@ def count_set_bits(byte):
         byte >>= 1
     return count
 
+def plot_coordinates(unique_XY_by_location):
+    colors = plt.cm.rainbow(np.linspace(0, 1, len(unique_XY_by_location)))
+    for (location, coordinates), color in zip(unique_XY_by_location.items(), colors):
+        X = [coord[0] for coord in coordinates]
+        Y = [coord[1] for coord in coordinates]
+        fig, ax = plt.subplots(1, figsize=(5,5), dpi=100)
+        ax.scatter(X, Y, alpha=0.6, edgecolors='w', color=color)
+        # Only add the label to the line plot
+        ax.plot(X, Y, alpha=0.5, color=color, label=f'Location {location}')
+        fig.savefig(f"testing_files/{location}/location_{location}.png")
+        fig.suptitle('Ordered XY Coordinates by Location')
+        ax.set_xlabel('X Coordinate')
+        ax.set_ylabel('Y Coordinate')
+    # Ensure the legend is displayed. The 'best' location will automatically adjust to a less obtrusive place.
+        fig.legend()
 
-pyboy = PyBoy("Pokemon - Crystal Version.gbc", window_scale=1)
+
+pyboy = PyBoy("emu_files/Pokemon - Crystal Version.gbc", window_scale=1)
 pyboy.set_emulation_speed(target_speed=0)
-unique_locations = set()
-unique_XY = set()
+unique_XY_by_location = {}
+imgs = {}
+
+with open("emu_files/states/start.state", "rb") as state:
+    pyboy.load_state(state)
 
 while not pyboy.tick():
     money_address = 0xD84E
     money_bytes = [pyboy.get_memory_value(money_address + i) for i in range(3)]
     money = bytes_to_int(money_bytes[::-1])
-    print(f"Player's Money: {money} (Raw bytes: {money_bytes})")
-    # DCB7 – Y coordinate on overworld map
-    # DCB8 – X coordinate on overworld map
+    print(f"Player's Money: {money} (Raw bytes: {money_bytes})\n")
+
     x_coord = pyboy.get_memory_value(0xDCB8)
     y_coord = pyboy.get_memory_value(0xDCB7)
     johto_badges = pyboy.get_memory_value(0xD857)
     kanto_badges = pyboy.get_memory_value(0xD858)
-    print(
-        f"Johto Badges: {johto_badges}, Kanto Badges: {kanto_badges} (Raw bytes: {[johto_badges, kanto_badges]})"
-    )
+    print(f"Johto Badges: {johto_badges}, Kanto Badges: {kanto_badges} (Raw bytes: {[johto_badges, kanto_badges]})\n")
 
     num_pokemon = pyboy.get_memory_value(0xDCD7)
-    print(f"Number of Party Pokémon: {num_pokemon} (Raw byte: {num_pokemon})")
+    print(f"Number of Party Pokémon: {num_pokemon} (Raw byte: {num_pokemon})\n")
 
     total_level = 0
     for i in range(num_pokemon):
@@ -82,33 +89,43 @@ while not pyboy.tick():
             * np.array([1, 256, 65536])
         )
         total_level += level
-        print(
-            f"Pokémon {i+1}: Level {level} (Level Raw: {level}, HP Raw: {hp_raw}, EXP Raw: {exp_raw})"
-        )
+        print(f"Pokémon {i+1}: Level {level} (Level Raw: {level}, HP Raw: {hp_raw}, EXP Raw: {exp_raw})\n")
+
     received = pyboy.get_memory_value(0xCF60)
-    print(f"Total Level of Party: {total_level}")
-    print(
-        f"Player's X Coordinate: {x_coord}, Player's Y Coordinate: {y_coord} Received: {received}"
-    )
-    print(f"Player's Location: {pyboy.get_memory_value(0xD148)}")
+    print(f"Total Level of Party: {total_level}\n")
+    print(f"Player's X Coordinate: {x_coord}, Player's Y Coordinate: {y_coord} Received: {received}\n")
+    print(f"Player's Location: {pyboy.get_memory_value(0xD148)}\n")
     caught_pokemon = sum(
         count_set_bits(pyboy.get_memory_value(0xDE99 + i)) for i in range(20)
     )
-    print(f"Total Number of Caught Pokémon: {caught_pokemon}")
-    unique_locations.add(pyboy.get_memory_value(0xD148))
-    unique_XY.add((x_coord, y_coord))
+    print(f"Total Number of Caught Pokémon: {caught_pokemon}\n")
+    current_location = pyboy.get_memory_value(0xD148)
 
-    # Print the contents of the screen's text
-    screen_image = pyboy.botsupport_manager().screen().screen_image()
-    screen_image = preprocess_image(screen_image)
-    text = extract_text(screen_image)
-    print(f"Text on screen: {text}")
+    if current_location not in unique_XY_by_location:
+        unique_XY_by_location[current_location] = []
 
-    # pass 60 ticks to let a full second pass
+    if current_location not in imgs:    
+        imgs[current_location] = []
+    unique_XY_by_location[current_location].append((x_coord, y_coord))
+    imgs[current_location].append(pyboy.botsupport_manager().screen().screen_image())
+
+    # screen_image = pyboy.botsupport_manager().screen().screen_image()
+    # screen_image = preprocess_image(screen_image)
+    # text = extract_text(screen_image)
+    #print(f"Text on screen: {text}\n")
+
     for i in range(60):
         pyboy.tick()
 
-print(f"Unique Locations: {unique_locations}")
-print(f"Unique XY Coordinates: {unique_XY}")
+
+if not os.path.exists("testing_files"):
+    os.makedirs("testing_files")
+
+for loc, img_list in imgs.items():
+    for i, img in enumerate(img_list):
+        if not os.path.exists(f"testing_files/{loc}"):
+            os.makedirs(f"testing_files/{loc}")
+        img.save(f"testing_files/{loc}/{loc}_{i}.png")
+plot_coordinates(unique_XY_by_location)
 
 pyboy.stop()
