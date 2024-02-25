c6f8775f82b5e88648e31d14fab2690c2947e004
AoifeHughes <ahughes@turing.ac.uk>
2024-02-25 16:43:54 +0000

    fix name

--- run.sh
+++ run.sh
@@ -1,34 +1,45 @@
 #!/bin/bash
 
-base_episode_length=256
-num_workers=8
+base_episode_length=16
+num_workers=6
 runs_per_worker=1
 
 # Array to hold grayscale usage flags
 use_grayscale_flags=("true" "false")
 
-# Outer loop to toggle grayscale flag
-for use_grayscale in "${use_grayscale_flags[@]}"
+# Array for scaling_factor values
+scaling_factors=(0.25 0.5 1)
+
+# Outermost loop for scaling_factor
+for scaling_factor in "${scaling_factors[@]}"
 do
-    for ((i=1; i<=5; i++))
+    # Outer loop to toggle grayscale flag
+    for use_grayscale in "${use_grayscale_flags[@]}"
     do
-        # Calculate episode_length for the current iteration
-        episode_length=$((base_episode_length * i))
+        for ((i=1; i<=5; i++))
+        do
+            # Calculate episode_length for the current iteration, adjusted by scaling_factor
+            episode_length=$(echo "$base_episode_length * $i * $scaling_factor" | bc)
+            episode_length=${episode_length%.*}  # Convert to integer if necessary
 
-        # Check if the directory already exists
-        if [ -d "run_${i}_grayscale_${use_grayscale}" ]; then
-            echo "Directory run_${i}_grayscale_${use_grayscale} already exists, skipping..."
-            continue
-        fi
+            # Define a directory name that includes scaling_factor
+            dir_name="run_${i}_scale_${scaling_factor}_grayscale_${use_grayscale}"
 
-        # Execute the Python script with dynamic episode_length and grayscale flag
-        python main.py --use_config ./configs/multi_config.json --episode_length $episode_length --num_workers $num_workers --runs_per_worker $runs_per_worker --use_grayscale $use_grayscale
+            # Check if the directory already exists
+            if [ -d "$dir_name" ]; then
+                echo "Directory $dir_name already exists, skipping..."
+                continue
+            fi
 
-        # Create directory and move files
-        mkdir "run_${i}_grayscale_${use_grayscale}"
-        mv results "run_${i}_grayscale_${use_grayscale}"
-        mv runs/* "run_${i}_grayscale_${use_grayscale}"
-        mv logs "run_${i}_grayscale_${use_grayscale}"
-        mv checkpoints "run_${i}_grayscale_${use_grayscale}"
+            # Execute the Python script with dynamic episode_length, grayscale flag, and scaling factor
+            python main.py --use_config ./configs/multi_config.json --episode_length $episode_length --num_workers $num_workers --runs_per_worker $runs_per_worker --use_grayscale $use_grayscale --scaling_factor $scaling_factor
+
+            # Create directory and move files
+            mkdir "$dir_name"
+            mv results "$dir_name"
+            mv runs/* "$dir_name"
+            mv logs "$dir_name"
+            mv checkpoints "$dir_name"
+        done
     done
 done
