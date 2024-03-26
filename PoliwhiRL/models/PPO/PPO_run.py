03f5704a48eb05a6bc1ecfe5cdf2eb047b54b16e
AoifeHughes <ahughes@turing.ac.uk>
2024-03-26 08:26:48 +0000

    Refactor PPO training functions and controller class

--- PoliwhiRL/models/PPO/PPO_run.py
+++ PoliwhiRL/models/PPO/PPO_run.py
@@ -2,7 +2,6 @@
 from .training_functions import (
     setup_environment_and_model,
     train,
-    run_eval,
     save_checkpoint,
 )
 
@@ -10,7 +9,6 @@ from .training_functions import (
 def setup_and_train_ppo(config):
     env, model, optimizer, start_episode = setup_environment_and_model(config)
     train(model, env, optimizer, config, start_episode)  # Train the model
-    run_eval(model, env, config)  # Evaluate after training
     save_checkpoint(
         model,
         config["checkpoint"],
