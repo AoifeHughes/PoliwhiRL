f2aa4a11d9d6d279dd6ee32a93b9564adcfafd75
AoifeHughes <ahughes@turing.ac.uk>
2024-03-28 09:05:46 +0000

    Fix replay chance in PPO training

--- PoliwhiRL/models/PPO/PPO_run.py
+++ PoliwhiRL/models/PPO/PPO_run.py
@@ -7,15 +7,12 @@ from .training_functions import (
 
 
 def setup_and_train_ppo(config):
-    if ',' in config['episode_length']:
-        ep_lengths = [int(i) for i in config['episode_length'].split(',')]
+    if "," in config["episode_length"]:
+        ep_lengths = [int(i) for i in config["episode_length"].split(",")]
     else:
-        ep_lengths = [int(config['episode_length'])]
-
-
-
+        ep_lengths = [int(config["episode_length"])]
     for ep_length in ep_lengths:
-        config['episode_length'] = ep_length
+        config["episode_length"] = ep_length
         env, model, optimizer, start_episode = setup_environment_and_model(config)
         train(model, env, optimizer, config, start_episode)  # Train the model
         save_checkpoint(
