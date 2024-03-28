1da6739fde70d1ffac8bc3141618f0231bb82574
AoifeHughes <ahughes@turing.ac.uk>
2024-03-28 08:40:32 +0000

    Update episode and loss configurations

--- PoliwhiRL/models/PPO/PPO_run.py
+++ PoliwhiRL/models/PPO/PPO_run.py
@@ -7,11 +7,20 @@ from .training_functions import (
 
 
 def setup_and_train_ppo(config):
-    env, model, optimizer, start_episode = setup_environment_and_model(config)
-    train(model, env, optimizer, config, start_episode)  # Train the model
-    save_checkpoint(
-        model,
-        config["checkpoint"],
-        start_episode + config["num_episodes"],
-    )
-    env.close()
+    if ',' in config['episode_length']:
+        ep_lengths = [int(i) for i in config['episode_length'].split(',')]
+    else:
+        ep_lengths = [int(config['episode_length'])]
+
+
+
+    for ep_length in ep_lengths:
+        config['episode_length'] = ep_length
+        env, model, optimizer, start_episode = setup_environment_and_model(config)
+        train(model, env, optimizer, config, start_episode)  # Train the model
+        save_checkpoint(
+            model,
+            config["checkpoint"],
+            start_episode + config["num_episodes"],
+        )
+        env.close()
