9d94584bcf22e0d94442caac8bb4aae91b28771c
AoifeHughes <ahughes@turing.ac.uk>
2024-04-11 13:45:45 +0100

    Update DQN agent to use parallel workers for training

--- PoliwhiRL/models/DQN/parallel_agent.py
+++ PoliwhiRL/models/DQN/parallel_agent.py
@@ -20,21 +20,26 @@ class ParallelDQNAgent(BaseDQNAgent):
         )
         self.workers = []
         self.reward_queues = [Queue() for _ in range(self.num_workers)]
+        self.work_queues = [Queue() for _ in range(self.num_workers)]
         for idx in range(self.num_workers):
-            worker = Worker(self.model, self.memory, self.config, idx, self.reward_queues[idx])
+            worker = Worker(self.model, self.memory, self.config, idx, self.reward_queues[idx], self.work_queues[idx])
             self.workers.append(worker)
 
     def train(self, num_episodes):
         rewards = []
+
+        # Start the worker processes
         for worker in self.workers:
             worker.start()
 
         for episode in tqdm(range(num_episodes), desc="Training"):
+            # Signal the workers to start a new episode
+            for queue in self.work_queues:
+                queue.put(("run", None))
+
             total_rewards = []
-            for worker, reward_queue in zip(self.workers, self.reward_queues):
-                worker.join()
+            for reward_queue in self.reward_queues:
                 total_rewards.append(reward_queue.get())
-                worker.reset()
 
             rewards.extend(total_rewards)
             batch = self.memory.sample(self.batch_size)
@@ -43,36 +48,52 @@ class ParallelDQNAgent(BaseDQNAgent):
             for worker in self.workers:
                 worker.model.load_state_dict(self.model.state_dict())
 
-        self.plot_progress(rewards, self.config["record_id"])
+            self.plot_progress(rewards, self.config["record_id"])
+
+        # Signal the workers to terminate
+        for queue in self.work_queues:
+            queue.put(("terminate", None))
+
+        # Wait for the worker processes to finish
         for worker in self.workers:
-            worker.terminate()
+            worker.join()
 
 class Worker(mp.Process):
-    def __init__(self, model, memory, config, worker_id, reward_queue):
+    def __init__(self, model, memory, config, worker_id, reward_queue, work_queue):
         super().__init__()
         self.model = model
         self.memory = memory
         self.config = config
         self.worker_id = worker_id
         self.reward_queue = reward_queue
+        self.work_queue = work_queue
         self.epsilon = config["epsilon_start"]
 
     def run(self):
         env = Env(self.config)
-        state = env.reset()
-        done = False
-        episode_reward = 0
 
-        while not done:
-            action = self.act(np.array([state]))
-            next_state, reward, done = env.step(action)
-            self.memory.add(state, action, reward, next_state, done, self.worker_id)
-            state = next_state
-            episode_reward += reward
+        while True:
+            signal, _ = self.work_queue.get()
 
-        #env.record(self.epsilon, f"dqn{self.worker_id}", 0, reward)
+            if signal == "terminate":
+                break
+
+            state = env.reset()
+            done = False
+            episode_reward = 0
+
+            while not done:
+                action = self.act(np.array([state]))
+                next_state, reward, done = env.step(action)
+                self.memory.add(state, action, reward, next_state, done, self.worker_id)
+                state = next_state
+                episode_reward += reward
+
+            #env.record(self.epsilon, f"dqn{self.worker_id}", 0, reward)
+            self.reward_queue.put(episode_reward)
+            self.epsilon = self.epsilon * self.config["epsilon_decay"]
+
         env.close()
-        self.reward_queue.put(episode_reward)
 
     def act(self, state_sequence):
         if np.random.rand() <= self.epsilon:
@@ -90,6 +111,3 @@ class Worker(mp.Process):
             action = torch.argmax(q_values[0, -1]).item()
 
         return action
-
-    def reset(self):
-        self.epsilon = self.epsilon * self.config["epsilon_decay"]
