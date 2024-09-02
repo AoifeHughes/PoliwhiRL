8a2f0184bd75736eb64f3aa933a585f84dc44e39
AoifeHughes <aoife.1.hughes@gmail.com>
2024-09-02 17:56:29 +0100

    on hold for now

--- PoliwhiRL/models/DQN/DQN.py
+++ PoliwhiRL/models/DQN/DQN.py
@@ -12,25 +12,25 @@ def setup_and_train(config):
     )
     num_actions = env.action_space.n
 
-    #agent = PokemonAgent(state_shape, num_actions, config, env)
-    agent = setup_and_train_multi_agent(state_shape, num_actions, config)
+    agent = PokemonAgent(state_shape, num_actions, config, env)
+    #agent = setup_and_train_multi_agent(state_shape, num_actions, config)
 
-    # model_path = config["checkpoint"]
-    # try:
-    #     agent.load_model(model_path)
-    #     print(f"Loaded model from {model_path}")
-    #     agent.optimizer = torch.optim.Adam(
-    #         agent.model.parameters(), lr=config["learning_rate"]
-    #     )
-    # except FileNotFoundError:
-    #     print("No model found, training from scratch.")
-    # except Exception as e:
-    #     print(f"Error loading model: {e}")
-    #     print("Training from scratch.")
+    model_path = config["checkpoint"]
+    try:
+        agent.load_model(model_path)
+        print(f"Loaded model from {model_path}")
+        agent.optimizer = torch.optim.Adam(
+            agent.model.parameters(), lr=config["learning_rate"]
+        )
+    except FileNotFoundError:
+        print("No model found, training from scratch.")
+    except Exception as e:
+        print(f"Error loading model: {e}")
+        print("Training from scratch.")
 
-    # num_episodes = config["num_episodes"]
-    # agent.train_agent(num_episodes)
+    num_episodes = config["num_episodes"]
+    agent.train_agent(num_episodes)
 
-    # # Final save
-    # agent.save_model(model_path)
-    # print(f"Saved model to {model_path}")
+    # Final save
+    agent.save_model(model_path)
+    print(f"Saved model to {model_path}")
