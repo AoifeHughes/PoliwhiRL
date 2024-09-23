fef0f8f934f21f1e3cdbbb9cf790d4bfe25730e6
AoifeHughes <aoife.1.hughes@gmail.com>
2024-09-23 11:34:45 +0100

    minor tweaks to db

--- PoliwhiRL/environment/rewards.py
+++ PoliwhiRL/environment/rewards.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 import numpy as np
-
+from collections import OrderedDict
 
 class Rewards:
     def __init__(self, config):
@@ -9,7 +8,7 @@ class Rewards:
         self.N_goals_target = config["N_goals_target"]
         self.break_on_goal = config["break_on_goal"]
         self.punish_steps = config["punish_steps"]
-        
+
         # Simplified reward values
         self.small_reward = 0.1
         self.medium_reward = 0.5
@@ -22,13 +21,16 @@ class Rewards:
         self.goal_reward_max = 10.0
         self.goal_reward_min = 1.0
 
+        # Clipping
+        self.clip = 20
+
         # Other rewards and penalties
         self.exploration_reward = self.small_reward
         self.step_penalty = self.small_penalty if self.punish_steps else 0
         self.button_penalty = self.medium_penalty
         self.pokedex_seen_reward = self.medium_reward
         self.pokedex_owned_reward = self.large_reward
-        
+
         # State variables
         self.pokedex_seen = 0
         self.pokedex_owned = 0
@@ -41,6 +43,13 @@ class Rewards:
         self.cumulative_reward = 0
         self.allowed_pokedex_goals = ["seen", "owned"]
 
+        # New variables for ordered goals
+        self.location_goals = OrderedDict()
+        self.current_goal_index = 0
+
+        # New parameter for distance-based reward
+        self.distance_reward_factor = self.medium_reward  
+
         self.set_goals(config["location_goals"], config["pokedex_goals"])
 
         if self.N_goals_target == -1:
@@ -52,7 +61,7 @@ class Rewards:
         self.done = False
 
     def set_goals(self, location_goals, pokedex_goals):
-        self.location_goals = {}
+        self.location_goals = OrderedDict()
         self.pokedex_goals = {}
         if location_goals:
             for idx, goal in enumerate(location_goals):
@@ -83,7 +92,7 @@ class Rewards:
 
         self.cumulative_reward += total_reward
 
-        return np.clip(total_reward, -10, 10), self.done
+        return np.clip(total_reward, -self.clip, self.clip), self.done
 
     def _check_goals(self, env_vars):
         reward = 0
@@ -96,7 +105,7 @@ class Rewards:
             env_vars["room"],
         )
         xyl = [cur_x, cur_y, cur_loc]
-        reward += self._check_goal_achievement(self.location_goals, xyl)
+        reward += self._check_goal_achievement(xyl)
 
         # Check pokedex goals
         for goal_type in ["seen", "owned"]:
@@ -107,17 +116,16 @@ class Rewards:
                     else self.pokedex_owned_reward
                 )
                 setattr(self, f"pokedex_{goal_type}", env_vars[f"pokedex_{goal_type}"])
-                reward += self._check_goal_achievement(
-                    self.pokedex_goals, env_vars[f"pokedex_{goal_type}"], goal_type
+                reward += self._check_pokedex_goal_achievement(
+                    env_vars[f"pokedex_{goal_type}"], goal_type
                 )
         return reward
 
-    def _check_goal_achievement(self, goals, current_value, goal_type=None):
-        for key, value in list(goals.items()):
-            if (goal_type is None and current_value in value) or (
-                goal_type and current_value >= value
-            ):
-                del goals[key]
+    def _check_goal_achievement(self, current_value):
+        if self.current_goal_index < len(self.location_goals):
+            goal = list(self.location_goals.values())[self.current_goal_index]
+            if current_value in goal:
+                self.current_goal_index += 1
                 self.N_goals += 1
                 # Calculate decaying reward
                 progress = self.steps / self.max_steps
@@ -126,21 +134,47 @@ class Rewards:
                 )
                 reward = max(self.goal_reward_min, min(self.goal_reward_max, reward))
                 if self.N_goals >= self.N_goals_target:
+                    reward *= 2  # Double reward for reaching all goals
                     if self.break_on_goal:
                         self.done = True
-                    return reward
                 return reward
+        return 0
+
+    def _check_pokedex_goal_achievement(self, current_value, goal_type):
+        if goal_type in self.pokedex_goals and current_value >= self.pokedex_goals[goal_type]:
+            del self.pokedex_goals[goal_type]
+            self.N_goals += 1
+            # Calculate decaying reward
+            progress = self.steps / self.max_steps
+            reward = self.goal_reward_max - (
+                (self.goal_reward_max - self.goal_reward_min) * progress
+            )
+            reward = max(self.goal_reward_min, min(self.goal_reward_max, reward))
+            if self.N_goals >= self.N_goals_target:
+                reward *= 2  # Double reward for reaching all goals
+                if self.break_on_goal:
+                    self.done = True
+            return reward
         return 0
 
     def _exploration_reward(self, env_vars):
         current_location = ((env_vars["X"], env_vars["Y"]), env_vars["map_num"])
         if current_location not in self.explored_tiles:
             self.explored_tiles.add(current_location)
-            return self.exploration_reward
+            dist_reward = self._distance_based_reward(env_vars)
+            return self.exploration_reward + dist_reward
         return 0
 
     def _step_penalty(self):
         return self.step_penalty
+
+    def _distance_based_reward(self, env_vars):
+        current_location = (env_vars["X"], env_vars["Y"])
+        goal = list(self.location_goals.values())[self.current_goal_index][0]  
+        if env_vars["map_num"] == goal[2]:  # Check if on the same map
+            distance = np.sqrt((current_location[0] - goal[0])**2 + (current_location[1] - goal[1])**2)
+            return self.distance_reward_factor * (1 / (distance + 1))  
+        return 0
 
     def get_progress(self):
         return {
